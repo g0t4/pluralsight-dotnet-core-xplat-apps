5d62ee16e36fbd3a3b8cdce6d4cd4aec647a2080
Wes Higbee <wes.mcclure@gmail.com>
2017-01-09 21:18:06 -0800

    Parse links with HtmlAgilityParser

---
+++ CheckLinksConsole/LinkChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+public class LinkChecker
+{
+	public static IEnumerable<string> GetLinks(string page)
+	{
+		var htmlDocument = new HtmlDocument();
+		htmlDocument.LoadHtml(page);
+		var links = htmlDocument.DocumentNode.SelectNodes("//a[@href]")
+			.Select(n => n.GetAttributeValue("href", string.Empty))
+			.Where(l => !String.IsNullOrEmpty(l))
+			.Where(l => l.StartsWith("http"));
+		return links;
+	}
+}
--- CheckLinksConsole/Program.cs
+++ CheckLinksConsole/Program.cs
@@ -1,13 +1,19 @@
 using System;
+using System.Linq;
 using System.Net.Http;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-	    var site = "https://g0t4.github.io/pluralsight-dotnet-core-xplat-apps";
-	    var client = new HttpClient();
-	    var body = client.GetStringAsync(site);
+	static void Main(string[] args)
+	{
+		var site = "https://g0t4.github.io/pluralsight-dotnet-core-xplat-apps";
+		var client = new HttpClient();
+		var body = client.GetStringAsync(site);
 		Console.WriteLine(body.Result);
-    }
+
+		Console.WriteLine();
+		Console.WriteLine("Links");
+		var links = LinkChecker.GetLinks(body.Result);
+		links.ToList().ForEach(Console.WriteLine);
+	}
 }

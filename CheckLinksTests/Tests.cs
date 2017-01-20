cbe5eef1ec22ff8e40aeeeded9b6b3127f3053fd
Wes Higbee <wes.mcclure@gmail.com>
2017-01-19 16:37:39 -0800

    Cleanup code

--- CheckLinksTests/Tests.cs
+++ CheckLinksTests/Tests.cs
@@ -4,14 +4,14 @@ using Xunit;
 
 namespace Tests
 {
-    public class Tests
-    {
-        [Fact]
-        public void WithoutHttpAtStartOfLink_NoLinks()
-        {
-	        var links = LinkChecker.GetLinks("<a href=\"google.com\" />");
-	        Assert.Equal(links.Count(), 0);
-        }
+	public class Tests
+	{
+		[Fact]
+		public void WithoutHttpAtStartOfLink_NoLinks()
+		{
+			var links = LinkChecker.GetLinks("<a href=\"google.com\" />");
+			Assert.Equal(links.Count(), 0);
+		}
 
 		[Fact]
 		public void WithHttpAtStartOfLink_LinkParses()

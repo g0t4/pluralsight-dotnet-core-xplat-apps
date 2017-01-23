346538d780732efa07fb06432e86b69aef631635
Wes Higbee <wes.mcclure@gmail.com>
2017-01-23 15:41:40 -0800

    Read our website from a command line argument

--- CheckLinksConsole/Program.cs
+++ CheckLinksConsole/Program.cs
@@ -2,11 +2,17 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using Microsoft.Extensions.Configuration;
 
 class Program
 {
 	static void Main(string[] args)
 	{
+		var configBuilder = new ConfigurationBuilder()
+			.AddCommandLine(args);
+		var configuration = configBuilder.Build();
+		var site = configuration["site"];
+		
 		var currentDirectory = Directory.GetCurrentDirectory();
 		var outputFolder = "reports";
 		var outputFile = "report.txt";
@@ -14,7 +20,7 @@ class Program
 		var directory = Path.GetDirectoryName(outputPath);
 		Directory.CreateDirectory(directory);
 		Console.WriteLine($"Saving report to {outputPath}");
-		var site = "https://g0t4.github.io/pluralsight-dotnet-core-xplat-apps";
+		//var site = "https://g0t4.github.io/pluralsight-dotnet-core-xplat-apps";
 		var client = new HttpClient();
 		var body = client.GetStringAsync(site);
 		Console.WriteLine(body.Result);

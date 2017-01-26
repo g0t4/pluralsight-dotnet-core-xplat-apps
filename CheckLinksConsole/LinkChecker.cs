de3ecc59b769079c6550f90e9b6056df700c0485
Wes Higbee <wes.mcclure@gmail.com>
2017-01-26 11:52:22 -0500

    Setup a DbContext and add properties to our LinkCheckResult for saving to a db

--- CheckLinksConsole/LinkChecker.cs
+++ CheckLinksConsole/LinkChecker.cs
@@ -61,8 +61,10 @@ public class LinkChecker
 
 public class LinkCheckResult
 {
+	public int Id { get; set; }
 	public bool Exists => String.IsNullOrWhiteSpace(Problem);
 	public bool IsMissing => !Exists;
 	public string Problem { get; set; }
 	public string Link { get; set; }
+	public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
---
+++ CheckLinksConsole/LinksDb.cs
@@ -0,0 +1,16 @@
+﻿
+namespace CheckLinksConsole
+{
+    using Microsoft.EntityFrameworkCore;
+
+	public class LinksDb : DbContext 
+	{
+        public DbSet<LinkCheckResult> Links {get;set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connection = @"Server=localhost;Database=Links;User Id=sa;Password=whatever12!";
+            optionsBuilder.UseSqlServer(connection);
+        }
+	}
+}

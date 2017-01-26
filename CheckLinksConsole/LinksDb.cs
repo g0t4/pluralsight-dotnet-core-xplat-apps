6c1d2ccd3aa08bd4b77f12add24564afe7976aad
Wes Higbee <wes.mcclure@gmail.com>
2017-01-26 14:52:10 -0500

    Switch to MySQL via Pomelo EF Core provider

--- CheckLinksConsole/LinksDb.cs
+++ CheckLinksConsole/LinksDb.cs
@@ -9,8 +9,13 @@ namespace CheckLinksConsole
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var connection = @"Server=localhost;Database=Links;User Id=sa;Password=whatever12!";
-            optionsBuilder.UseSqlServer(connection);
+            // MSSQL:
+            //var connection = @"Server=localhost;Database=Links;User Id=sa;Password=whatever12!";
+            //optionsBuilder.UseSqlServer(connection);
+
+            // MySQL (Pomelo):
+            var connection = "server=localhost;userid=root;pwd=password;database=Links;sslmode=none;";
+            optionsBuilder.UseMySql(connection);
         }
 	}
 }

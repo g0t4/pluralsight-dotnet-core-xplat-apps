c10ed1a0925766521baea6cd3bdbedc43c53369c
Wes Higbee <wes.mcclure@gmail.com>
2017-01-27 10:05:59 -0800

    Setup hangfire for jobs and have dashboard in web endpoint, move to a job that is DI'd via aspnet core DI and hangfire setup

--- CheckLinksConsole/Program.cs
+++ CheckLinksConsole/Program.cs
@@ -5,17 +5,35 @@ using System.Net.Http;
 
 namespace CheckLinksConsole
 {
+	using Hangfire;
+	using Hangfire.MemoryStorage;
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.Extensions.Configuration;
+	using Microsoft.Extensions.DependencyInjection;
 	using Microsoft.Extensions.Logging;
 
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			//GlobalConfiguration.Configuration.UseMemoryStorage();
+
+			var host = new WebHostBuilder()
+			   .UseKestrel()
+			   .UseContentRoot(Directory.GetCurrentDirectory())
+			   .UseStartup<Startup>()
+			   .Build();
+			RecurringJob.AddOrUpdate<CheckLinks>(c => c.Check(), Cron.Minutely);
+
+			host.Run();
+
+			return;
 			var config = new Config(args);
 			Logs.Init(config.ConfigurationRoot);
 			var logger = Logs.Factory.CreateLogger<Program>();
 			Directory.CreateDirectory(config.Output.GetReportDirectory());
-			
+
 			logger.LogInformation(200, $"Saving report to {config.Output.GetReportFilePath()}");
 			var client = new HttpClient();
 			var body = client.GetStringAsync(config.Site);
@@ -37,4 +55,64 @@ namespace CheckLinksConsole
 			}
 		}
 	}
+
+	public class CheckLinks
+	{
+		private readonly ILogger<CheckLinks> _Logger;
+
+		public CheckLinks(ILogger<CheckLinks> logger)
+		{
+			_Logger = logger;
+		}
+
+		public void Check()
+		{
+			_Logger.LogInformation("\n\n\nChecking\n\n\n");
+		}
+	}
+
+
+	public class Startup
+	{
+		public Startup(IHostingEnvironment env)
+		{
+			var builder = new ConfigurationBuilder()
+				.AddEnvironmentVariables();
+			Configuration = builder.Build();
+		}
+
+		public IConfigurationRoot Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+			services.AddHangfire(x => x.UseMemoryStorage());
+			services.AddTransient<CheckLinks>();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole();
+
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+			app.UseHangfireServer();
+			app.UseHangfireDashboard();
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+		}
+	}
+
 }

abfa2eb41220dd9e78459f9782bbbf68b456ae4a
Wes Higbee <wes.mcclure@gmail.com>
2017-01-31 19:59:24 -0500

    format code

--- CheckLinksConsole/Program.cs
+++ CheckLinksConsole/Program.cs
@@ -4,34 +4,34 @@ namespace CheckLinksConsole
     using System;
     using System.IO;
     using Hangfire;
-	using Hangfire.MemoryStorage;
+    using Hangfire.MemoryStorage;
     using Microsoft.AspNetCore.Hosting;
 
     class Program
-	{
-		static void Main(string[] args)
-		{
-			var config = new Config(args);
-			Logs.Init(config.ConfigurationRoot);
+    {
+        static void Main(string[] args)
+        {
+            var config = new Config(args);
+            Logs.Init(config.ConfigurationRoot);
 
-			GlobalConfiguration.Configuration.UseMemoryStorage();
+            GlobalConfiguration.Configuration.UseMemoryStorage();
 
-			RecurringJob.AddOrUpdate<CheckLinkJob>("check-link", j => j.Execute(config.Site, config.Output), Cron.Minutely);
-			RecurringJob.Trigger("check-link");
+            RecurringJob.AddOrUpdate<CheckLinkJob>("check-link", j => j.Execute(config.Site, config.Output), Cron.Minutely);
+            RecurringJob.Trigger("check-link");
 
-			var host = new WebHostBuilder()
+            var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .Build();
 
-			using (var server = new BackgroundJobServer())
-			{
-				Console.WriteLine("Hangfire Server started.");
-				//Console.ReadKey();
-				host.Run();
-			}
+            using (var server = new BackgroundJobServer())
+            {
+                Console.WriteLine("Hangfire Server started.");
+                //Console.ReadKey();
+                host.Run();
+            }
 
-		}
-	}
+        }
+    }
 }

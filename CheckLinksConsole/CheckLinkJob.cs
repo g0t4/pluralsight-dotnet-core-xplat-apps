906d1603dcd324632be4e41917653e1e653a60c1
Wes Higbee <wes.mcclure@gmail.com>
2017-02-07 21:40:35 -0500

    Use ctor injection to access a logger in CheckLinkJob

--- CheckLinksConsole/CheckLinkJob.cs
+++ CheckLinksConsole/CheckLinkJob.cs
@@ -1,36 +1,44 @@
 namespace CheckLinksConsole
 {
-	using System.IO;
-	using System.Linq;
-	using System.Net.Http;
-	using Microsoft.Extensions.Logging;
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Net.Http;
+    using Microsoft.Extensions.Logging;
 
-	public class CheckLinkJob
-	{
-		public void Execute(string site, OutputSettings output)
-		{
-			var logger = Logs.Factory.CreateLogger<CheckLinkJob>();
-			Directory.CreateDirectory(output.GetReportDirectory());
+    public class CheckLinkJob
+    {
+        private ILogger _Logger;
 
-			logger.LogInformation(200, $"Saving report to {output.GetReportFilePath()}");
-			var client = new HttpClient();
-			var body = client.GetStringAsync(site);
-			logger.LogDebug(body.Result);
+        public CheckLinkJob(ILogger<CheckLinkJob> logger)
+        {
+            _Logger = logger;
+            _Logger.LogInformation($"{Guid.NewGuid()}");
+        }
 
-			var links = LinkChecker.GetLinks(site, body.Result);
+        public void Execute(string site, OutputSettings output)
+        {
+            Directory.CreateDirectory(output.GetReportDirectory());
 
-			var checkedLinks = LinkChecker.CheckLinks(links);
-			using (var file = File.CreateText(output.GetReportFilePath()))
-			using (var linksDb = new LinksDb())
-			{
-				foreach (var link in checkedLinks.OrderBy(l => l.Exists))
-				{
-					var status = link.IsMissing ? "missing" : "OK";
-					file.WriteLine($"{status} - {link.Link}");
-					linksDb.Links.Add(link);
-				}
-				linksDb.SaveChanges();
-			}
-		}
-	}
+            _Logger.LogInformation(200, $"Saving report to {output.GetReportFilePath()}");
+            var client = new HttpClient();
+            var body = client.GetStringAsync(site);
+            _Logger.LogDebug(body.Result);
+
+            var links = LinkChecker.GetLinks(site, body.Result);
+
+            var checkedLinks = LinkChecker.CheckLinks(links);
+            using (var file = File.CreateText(output.GetReportFilePath()))
+            using (var linksDb = new LinksDb())
+            {
+                foreach (var link in checkedLinks.OrderBy(l => l.Exists))
+                {
+                    var status = link.IsMissing ? "missing" : "OK";
+                    file.WriteLine($"{status} - {link.Link}");
+                    linksDb.Links.Add(link);
+                }
+                linksDb.SaveChanges();
+            }
+        }
+    }
 }
